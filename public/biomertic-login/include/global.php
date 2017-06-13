d093a5f1d2e4d14dddd478e4f5f9739eedc02d06
aditya-dikonia <aditya.bansal@dikonia.in>
2017-06-13 16:34:08 +0530

    Update biometric config

--- public/biomertic-login/include/global.php
+++ public/biomertic-login/include/global.php
@@ -2,11 +2,11 @@
 	/*ini_set("display_errors", 0);
 	error_reporting(0);*/
 
-	$base_path		= "http://103.54.100.36:4104/weblogin/public/biomertic-login/";
-	$application_path		= "http://103.54.100.36:4104/weblogin/public/";
+	$base_path		= "http://onebusiness.shacknet.biz/OneBusiness/public/biomertic-login/";
+	$application_path		= "http://onebusiness.shacknet.biz/OneBusiness/public/";
 	$db_name		= "weblogindb";
-	$db_user		= "weblogindb";
-	$db_pass		= "sk)@!ggc1^12";
+	$db_user		= "aditya";
+	$db_pass		= "aditya0607";
 	$db_host		= "localhost";
 	$time_limit_reg = "15";
 	$time_limit_ver = "10";
