8a42075869771b93a187a525196b48232eb1e867
Vineet <vineet.kumar@dikonia.in>
2017-11-08 14:09:23 +0530

    Change database dynamically

---
+++ app/POTemplateDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class POTemplateDetail extends Model
+{
+    public $timestamps = false;
+    protected $table = "s_po_tmpl8_detail";
+    protected $primaryKey = "po_tmpl8_id";
+    protected $connection = 'mysql2';
+}
