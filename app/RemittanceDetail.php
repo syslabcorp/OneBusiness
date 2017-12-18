a46f075b024adb723018a7f6a202544e48e9c0b8
chutuan <chutuan020693@gmail.com>
2017-12-18 14:43:16 +0700

    #M09 Update

--- app/RemittanceDetail.php
+++ app/RemittanceDetail.php
@@ -36,7 +36,7 @@ class RemittanceDetail extends Model
       $shifts = $shifts->where("{$remittanceModel->getTable()}.Sales_Checked", '=', $queries['status']);
     }
 
-    if($queries['remarks_only'] == 1 && $company->corp_type == 'ICAFE') {
+    if($queries['remarks_only'] == 1) {
       $shifts = $shifts->whereNotNull("{$remittanceModel->getTable()}.Notes")
                        ->where("{$remittanceModel->getTable()}.Notes", "<>", "");
     }
