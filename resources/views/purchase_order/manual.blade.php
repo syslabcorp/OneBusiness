11100ec875058fb60b161a944c04f4b85be97e69
chutuan <chutuan020693@gmail.com>
2018-03-19 14:54:20 +0700

    update process manual

--- resources/views/purchase_order/manual.blade.php
+++ resources/views/purchase_order/manual.blade.php
@@ -35,7 +35,7 @@
         <div class="row purchase_header_form">
           <div class="row">
             <div class="col-md-12">
-              <form class="form-inline" action="/action_page.php">
+              <form class="form-inline" >
                 <div id="city-list" class="form-group">
                   <label>City</label>
                   <select class="form-control" style="width: 300px;" name="" id="">
@@ -53,99 +53,104 @@
           </div>
         </div>
         <div class="row purchase_choose">
-          <div class="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h5>Branch</h5>
+          <form action="{{ route('purchase_order.manual_suggest',['corpID' => $corpID]) }}" id="manual_form">
+            <input type="hidden" name="corpID" value="{{$corpID}}">
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h5>Branch</h5>
+                </div>
+
+                <div class="panel-body first">
+                  <div>
+                    <ul class="" id="branch">
+                      
+                    </ul>
+
+                  </div>
+
+                </div>
               </div>
 
-              <div class="panel-body first">
-                <div>
-                  <ul class="selectable" id="branch">
-                    
+                <div class="form-group">
+                  <div class="col-md-4">
+                    <label class="checkbox-inline">
+                      <input type="checkbox" id="NX" name="branch_type" value="NX" checked >NetExpress
+                    </label>
+                  </div>
+                  <div class="col-md-4">
+                    <label class="checkbox-inline">
+                      <input type="checkbox" id="SQ" name="branch_type" value="SQ" checked>Sequel
+                    </label>
+                  </div>
+                  <div class="col-md-4">
+                    <label class="checkbox-inline">
+                      <input type="checkbox" id="IS" name="branch_type" value="IS" checked>iSing
+                    </label>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label>Date Range</label>
+                  
+                <div class="row">
+                  <div class="col-md-6">
+                    <label for="">From</label>
+                    <input type="date" name="from_date" id="" class="datepicker" >
+                  </div>
+
+                  <div class="col-md-6">
+                    <label for="">To</label>
+                    <input type="date" name="to_date" id="" class="datepicker">
+                  </div>
+                </div>
+
+                </div>
+                <div class="form-group">
+                  <div class="col-md-3">
+                    <label class="">Multiplier</label>
+                  </div>
+                  <div class="col-md-9">
+                    <input class="form-control" name="multiolier" type="text">
+                  </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h5>Product Line</h5>
+                </div>
+
+                <div class="panel-body">
+                  <ul>
+                    @foreach($prodlines as $prodline)
+                      <li class="ui-widget-content">
+                        <div class="col-xs-2 text-center" style="border-right: 1px solid #aaaaaa">
+                          <input type="checkbox" class="prodline_item" value="{{$prodline->ProdLine_ID}}">
+                        </div>
+                        <div class="col-xs-10" style="white-space:nowrap;">
+                          <label class="label-control">{{ $prodline->Product }}</label> 
+                        </div>
+                      </li>
+                    @endforeach
                   </ul>
-
                 </div>
-
               </div>
             </div>
 
-            <form action="">
-              <div class="form-group">
-                <div class="col-md-4">
-                  <label class="checkbox-inline">
-                    <input type="checkbox" id="NX" name="branch_type" value="NX" checked >NetExpress
-                  </label>
-                </div>
-                <div class="col-md-4">
-                  <label class="checkbox-inline">
-                    <input type="checkbox" id="SQ" name="branch_type" value="SQ" checked>Sequel
-                  </label>
-                </div>
-                <div class="col-md-4">
-                  <label class="checkbox-inline">
-                    <input type="checkbox" id="IS" name="branch_type" value="IS" checked>iSing
-                  </label>
-                </div>
-              </div>
-              <div class="form-group">
-                <label>Date Range</label>
-                
-              <div class="row">
-                <div class="col-md-6">
-                  <label for="">From</label>
-                  <input type="date" name="" id="" class="datepicker" >
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h5>Item Code</h5>
                 </div>
 
-                <div class="col-md-6">
-                  <label for="">To</label>
-                  <input type="date" name="" id="" class="datepicker">
+                <div class="panel-body">
+                  <ul class="selectable" id="item_code">
+                  </ul>
                 </div>
-              </div>
-
-              </div>
-              <div class="form-group">
-                <div class="col-md-3">
-                  <label class="">Multiplier</label>
-                </div>
-                <div class="col-md-9">
-                  <input class="form-control" type="text">
-                </div>
-              </div>
-            </form>
-          </div>
-
-          <div class="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h5>Product Line</h5>
-              </div>
-
-              <div class="panel-body">
-                <ul>
-                  @foreach($prodlines as $prodline)
-                    <li class="ui-widget-content">
-                      <label class="label-control"><input type="checkbox" class="prodline_item" value="{{$prodline->ProdLine_ID}}">{{ $prodline->Product }}</label>
-                    </li>
-                  @endforeach
-                </ul>
               </div>
             </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h5>Item Code</h5>
-              </div>
-
-              <div class="panel-body">
-                <ul class="selectable" id="item_code">
-                </ul>
-              </div>
-            </div>
-          </div>
-
+          </form>
         </div>
 
       </div>
@@ -153,7 +158,7 @@
       <div class="panel-footer">
         <div class="row">
         <div class="pull-right">
-          <button class="btn btn-success">Generate PO</button>
+          <button id="manual_generate" class="btn btn-success">Generate PO</button>
         </div>
 
         </div>
