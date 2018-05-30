11c59218f15428ec3a21abe303337e92a743140b
chutuan <chutuan020693@gmail.com>
2018-05-30 11:46:05 +0700

    M20 fix error

--- resources/views/stocktransfer/create.blade.php
+++ resources/views/stocktransfer/create.blade.php
@@ -439,7 +439,7 @@
       }
       else
       {
-        if(!isItemRowsValid()) {
+        if(!isItemRowsValid() || $(this).parents('tr').find('.error').length > 0) {
           return false
         }
 
