1cdbb85a78fb79319048d1bc28d31ae7e4a0c0f6
chutuan <chutuan020693@gmail.com>
2018-11-13 10:06:17 +0700

    fix issue #6

--- app/Transformers/Stock/StockTransformer.php
+++ app/Transformers/Stock/StockTransformer.php
@@ -9,10 +9,10 @@ class StockTransformer extends Fractal\TransformerAbstract
     public function transform(Stock $stock)
     {
         return [
-            'txn_no' => $stock ? (int) $stock->txn_no : ''
-            // 'RR_No' =>  $stock ? (int) $stock->RR_No : '',
-            // 'RcvDate' => $stock->RcvDate ? $stock->RcvDate->format('M,d,Y') : '',
-            // 'TotalAmt' => $stock ? number_format($stock->TotalAmt,2) : '',
+            'txn_no' => $stock ? (int) $stock->txn_no : '',
+            'RR_No' =>  $stock ? (int) $stock->RR_No : '',
+            'RcvDate' => $stock->RcvDate ? $stock->RcvDate->format('M,d,Y') : '',
+            'TotalAmt' => $stock ? number_format($stock->TotalAmt,2) : ''
             // 'Payment_ID' => $stock ? $stock->Payment_ID : '',
             // 'VendorName' => $stock->vendor ? $stock->vendor->VendorName : '',
             // 'DateSaved' => $stock->DateSaved ? $stock->DateSaved->format('M,d,Y h:m:s A') : '',
