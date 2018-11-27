ce8671e0dd33771b94b49e2e97ca59e29aab6f99
chutuan <chutuan020693@gmail.com>
2018-11-27 15:30:28 +0700

    fix stock update

--- resources/views/stocks/stocks-item.blade.php
+++ resources/views/stocks/stocks-item.blade.php
@@ -20,16 +20,16 @@
       <tr class="stockRow">
         <td class="text-center">
           <input type="hidden" class="item_id" name="stocks[{{ $loop->index+1 }}][item_id]" value="{{ $row->item_id }}" >
-          <input type="text" class="form-control text-center item_code showSuggest" name="stocks[{{ $loop->index+1 }}][item_code]" value="{{ $row->item->ItemCode }}" >
+          <input type="text" class="form-control text-center item_code showSuggest" name="stocks[{{ $loop->index+1 }}][item_code]" value="{{ $row->item ? $row->item->ItemCode : '' }}" >
         </td>
-        <td><input type="text" data-column="product_line" class="form-control text-center showSuggest" name="stocks[{{ $loop->index+1 }}][product_line]" value="{{ $row->item->product_line->Product ? $row->item->product_line->Product : '' }}" autocomplete="off"></td>
-        <td><input type="text" data-column="brand" class="form-control text-center showSuggest" name="stocks[{{ $loop->index+1 }}][brand]" value="{{ $row->item->brand->Brand ? $row->item->brand->Brand : '' }}" autocomplete="off"></td>
-        <td class="text-center"><label >{{ $row->item->Description ? $row->item->Description : '' }}</label></td>
+        <td><input type="text" data-column="product_line" class="form-control text-center showSuggest" name="stocks[{{ $loop->index+1 }}][product_line]" value="{{ $row->item ? $row->item->product_line->Product : '' }}" autocomplete="off"></td>
+        <td><input type="text" data-column="brand" class="form-control text-center showSuggest" name="stocks[{{ $loop->index+1 }}][brand]" value="{{ $row->item ? $row->item->brand->Brand : '' }}" autocomplete="off"></td>
+        <td class="text-center"><label >{{ $row->item ? $row->item->Description : '' }}</label></td>
         <td class="text-center"><label >{{ $row->ServedQty }}</label></td>
         <td><input type="text"   class="form-control text-center " name="stocks[{{ $loop->index+1 }}][cost]" value="{{ $row->Cost ? $row->Cost*100/100 : '' }}" autocomplete="off"></td>
         <td><input type="number" class="form-control text-center  quantity" name="stocks[{{ $loop->index+1 }}][qty]" value="{{ $row->Qty ? $row->Qty : ''}}" autocomplete="off"></td>
         <td><input type="text"   class="form-control text-center subtotal" name="stocks[{{ $loop->index+1 }}][subtotal]" value="{{ $row->Cost ? ($row->Cost*100/100)*$row->Qty : '' }}" autocomplete="off"></td>
-        <td class="text-center"><label >{{ $row->item->Unit ? $row->item->Unit : '' }}</label></td>
+        <td class="text-center"><label >{{ $row->item ? $row->item->Unit : '' }}</label></td>
 
         <td style="width: 100px;">
           <button type="button" class="btn btn-danger btn-md btnRemoveRow center-block" >
